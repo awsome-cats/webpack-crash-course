61ec9b871f4a0bb37b64e90af1459b47a271a206
awesome-cats <cattwin82@gmail.com>
2020-04-06 11:51:56 +0900

    add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,9 @@ module.exports = {
     },
 module: {
    rules: [{ test:/\.css$/, use: ['style-loader', 'css-loader']},
+    { test:/\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
     {test: /\.(jpe?g|png|gif|svg|ico)$/i,loader: 'url-loader',
-    options: {limit: 2048, name: './images/[name].[ext]'}
-}
+    options: {limit: 2048, name: './images/[name].[ext]'}}
 ]
 },
     devServer: {
